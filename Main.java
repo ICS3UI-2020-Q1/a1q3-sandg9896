2378c1c7f9991f9c736b92393dfa0b43e8d1299a
sandg9896 <sandg9896@wrdsb.ca>
2020-09-14 15:56:28 +0000

    Mrs. Lush's Computer Parts

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Gavin Sandhar
  */
 public class Main {
 
@@ -9,7 +10,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // calculate how much it would cost to purchase different quantities of computer parts
+   String name;
+   System.out.println("Welcome! Enter which computer parts you would like to purchase along with a quantity:");
+   System.out.println("How many Chromebook Chargers would you like?:");
+    // Creates a Scanner used for input
+    Scanner input = new Scanner(System.in);
+     int chromebookCharger = input.nextInt();
+
+
+    System.out.println("How many Replacement Motherboards would you like?:");   
+     int replacementMotherboard = input.nextInt();
+    
+    System.out.println("How many Computer Mouses would you like?:");
+     int computerMouse = input.nextInt();
+
+     double Subtotal = (chromebookCharger*34.99 + replacementMotherboard*127.50 + computerMouse*18.00);
+
+     double Tax = (1.13);
+
+     // declare and calculate the quotient
+   System.out.println(Subtotal*1.13);
+
+
+
+    
     
   }
 }
